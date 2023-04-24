b49f504d5243986b123b1dda3f881a9fedb8ece7
Babilinx <babilinx.evx1o@simplelogin.com>
2023-04-24 16:44:56 +0000

    feat: Add radio class from maqueen.py
    This class handle the radio connection with others micro:bits.

--- feu.py
+++ feu.py
@@ -34,3 +34,39 @@ class Led:
         pin0.write_analog(red*4)
         pin1.write_analog(green*4)
         pin2.write_analog(blue*4)
+
+
+class Radio:
+    """Handle the radio communications with some log prints.
+    """
+
+    def __init__(self, channel: int,  power: int = 7):
+        config(channel=channel, power=power)
+        on()
+        print("Radio: Config radio on channel {} and power {}".format(channel, power))
+
+    def send(self, message: str):
+        """Send a string threw Bluetooth.
+
+        Args:
+            message (str): Message to send.
+
+        Status: Not tested
+        """
+
+        send(message)
+        print("Radio: Send message {}".format(message))
+
+    def receive(self) -> str:
+        """Return strings that habe been receive threw Bluetooth.
+
+        Returns:
+            str: Received message.
+
+        Status: Not tested
+        """
+
+        message = receive()
+        print("Radio: Recieve {}".format(message))
+        return message
+    
