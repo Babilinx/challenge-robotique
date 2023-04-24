56e9f4eaf9c6f5dff48bf150356981c6ba138011
Babilinx <babilinx.evx1o@simplelogin.com>
2023-04-24 17:12:36 +0000

    style: Formating with pyink

--- feu.py
+++ feu.py
@@ -1,19 +1,10 @@
-from microbit import (
-    pin0, pin1, pin2
-)
+from microbit import pin0, pin1, pin2
 
-from radio import (
-    config,
-    send,
-    receive,
-    on,
-    # off,
-)
+from radio import config, send, receive, on
 
 
 class Led:
-    """Handle LEDs
-    """
+    """Handle LEDs"""
 
     def rgb(self, red: int, green: int, blue: int):
         """Set the color of the LED using RGB values
@@ -31,16 +22,15 @@ class Led:
         #         | G
         #        GND
 
-        pin0.write_analog(red*4)
-        pin1.write_analog(green*4)
-        pin2.write_analog(blue*4)
+        pin0.write_analog(red * 4)
+        pin1.write_analog(green * 4)
+        pin2.write_analog(blue * 4)
 
 
 class Radio:
-    """Handle the radio communications with some log prints.
-    """
+    """Handle the radio communications with some log prints."""
 
-    def __init__(self, channel: int,  power: int = 7):
+    def __init__(self, channel: int, power: int = 7):
         config(channel=channel, power=power)
         on()
         print("Radio: Config radio on channel {} and power {}".format(channel, power))
@@ -69,4 +59,3 @@ class Radio:
         message = receive()
         print("Radio: Recieve {}".format(message))
         return message
-    
