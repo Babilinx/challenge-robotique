18f95381acb911faedfcdedccf8fce97036784e9
Babilinx <babilinx.evx1o@simplelogin.com>
2023-05-02 09:09:37 +0000

    feat: Add Radio class and data sending

--- controler.py
+++ controler.py
@@ -1,4 +1,5 @@
-from microbit import pin1, pin2, pin15, button_b
+from microbit import pin1, pin2, pin15, button_b, sleep
+from radio import config, send, receive
 
 
 class Controler:
@@ -23,3 +24,64 @@ class Controler:
 
     def get_button_B(self) -> bool:
         return button_b.is_pressed()
+
+
+class Radio:
+    """Handle the radio communications with some log prints."""
+
+    def __init__(self, power: int = 7):
+        config(channel=6, power=power)
+        on()
+        print("Radio: Config radio on channel {} and power {}".format("6", power))
+
+    def send(self, message: str):
+        """Send a string threw Bluetooth.
+
+        Args:
+            message (str): Message to send.
+
+        Status: Not tested
+        """
+
+        send(message)
+        print("Radio: Send message {}".format(message))
+
+    def receive(self) -> str:
+        """Return strings that habe been receive threw Bluetooth.
+
+        Returns:
+            str: Received message.
+
+        Status: Not tested
+        """
+
+        message = receive()
+        print("Radio: Recieve {}".format(message))
+        return message
+
+
+def main():
+    while True:
+        # Get data
+        analog_joystick_x = Controler.get_analog_joystick_x()
+        analog_joystick_y = Controler.get_analog_joystick_y()
+
+        joystick_x = Controler.convert_joystick_to_motor(analog_joystick_x)
+        joystick_y = Controler.convert_joystick_to_motor(analog_joystick_y)
+
+        button_b = Controler.get_button_B()
+        red_button = Controler.get_red_button()
+
+        # Send Data to Maqueen
+        Radio.send("Controller.joystick:{}|{}".format(joystick_x, joystick_y))
+        Radio.send("Controller.button_b:{}".format(button_b))
+        Radio.send("Controller.red_button:{}".format(red_button))
+
+        sleep(1 / 60)
+
+
+if __name__ == "__main__":
+    Radio = Radio()
+    Controler = Controler()
+
+    main()
